b14cfbe1ecae5b401af4e5859f4be6323e00fd20
Kolosov Ihor <108092986+Ak1yamaKiyoshi@users.noreply.github.com>
2023-12-15 21:53:47 +0200

    1

--- src/lab_8/t1.cpp
+++ src/lab_8/t1.cpp
@@ -1,12 +1,64 @@
 #include <string>
-#include <sstream>
 #include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <sstream>
+#include <Windows.h>
 
+/*
+10. У рядку символів визначити слова, які можна одержати перевертанням слова, що
+вводиться з клавіатури на запит користувача. Кожне друге слово розбити на склади.
+*/
 
 int main() {
-    std::string s;
-    getline(std::cin, s);
+    std::vector<std::string> dictionaryArray;
+    std::string line, word, target, syllable;
 
+    std::stringstream output;
+    const std::string vowels = "aeiouAEIOU";
+    const std::string consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
+
+    // Read dictionary from file
+    std::ifstream dictionaryFile("dictionary.dict", std::ios::in);
+    if (dictionaryFile.is_open())
+        while (getline(dictionaryFile, line))
+            dictionaryArray.push_back(line);
+    else return -1;
+
+    // Display first words for testing purposes
+    std::cout << " > Dictionary loaded: " << dictionaryArray.size() << " words" << std::endl;
+    std::cout << " > First 10 words: " << std::endl;
+    for (int i = 0; i < 10; i++) std::cout << dictionaryArray[i] << std::endl;
+
+    // Get input from user
+    std::cout << " > Enter bunch of words: ";
+    getline(std::cin, line);
+    std::istringstream iss(line);
+
+    while (iss >> word) {
+        target = word;
+        std::reverse(target.begin(), target.end());
+        // if reversed string in dictionary
+        if (std::find(dictionaryArray.begin(), dictionaryArray.end(), target) != dictionaryArray.end()) {
+            std::cout << " > Found: " << target << std::endl;
+            output << target << " ";
+        }
+        else { // break it into syllables
+            for (size_t i = 0; i < word.size(); ++i) {
+                syllable += word[i];
+                if (vowels.find(word[i]) != std::string::npos && i + 1 < word.size() && consonants.find(word[i + 1]) != std::string::npos) {
+                    output << syllable;
+                    syllable.clear();
+                    // If the next character is not the end of the word, add a "-"
+                    if (i != word.size() - 1) output << "-";
+                }
+            }
+            if (!syllable.empty()) output << syllable << " ";
+        }
+    }
+
+    std::cout << " > Result: " << output.str() << std::endl;
 
     return 0;
 }
--- src/lab_9/main.cpp
+++ src/lab_9/main.cpp
@@ -1,24 +1,53 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <stdlib.h>
+#include <time.h>
 
+const std::string FILENAME = "sample.txt";
+const std::string FILENAMEFINAL = "final.txt";
+
+
+void writefile(int lines) {
+    std::ofstream file(FILENAME);
+    for (int i = 0; i < lines; i++) {
+        file << "\n";
+        for (int j = 0; j < 10; j++)
+            file << static_cast<char>((rand() % 26) + 97);
+    }
+}
 
 int main() {
+    srand(time(NULL));
+    int sameStart = 0, sameBoth = 0;
+    char c;
 
-    char selected;
+    writefile(10000);
+    std::fstream file(FILENAME);
+    std::string line;
+    std::string *lines = new std::string[10000];
+
     std::cout << "Enter a character: ";
-    std::cin>>selected;
-    fstream f;
-    f.open("file.txt",ios::in);
-    if(!f) {
-        cout<<"Error in opening file";
-        return -1;    
-    } else if (f) {
-        std::string line; 
-        bool flag = false;
-        while(getline(line)) {
-            
+    std::cin >> c;
+
+    int i = 0;
+    while (std::getline(file, line)) {
+        if (line[0] == c) {
+            sameStart++;
+            lines[i++] = "same_start;" + line + "\n";
         }
-        f.close();
+        if (line[0] == c && line[line.length() - 1] == c) {
+            sameBoth++;
+            lines[i++] = "same_start&end;" + line + "\n";
+        }
     }
+
+    std::cout << "Lines starting with " << c << ": " << sameStart << std::endl;
+    std::cout << "Lines starting and ending with " << c << ": " << sameBoth << std::endl;
+
+    std::ofstream filefinal(FILENAMEFINAL);
+    i = 0;
+    while(lines[i++] != "")
+        filefinal << lines[i];
     return 0;
 }
